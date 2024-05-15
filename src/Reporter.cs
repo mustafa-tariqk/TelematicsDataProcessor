64f7b201892c4a5adb186c8545b9dc51330d4348
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-05-15 01:30:01 -0400

    concurrency fixed on all fronts

--- src/Reporter.cs
+++ src/Reporter.cs
@@ -2,78 +2,75 @@ using System;
 using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using static DataProcessor;
 
 class Reporter
 {
-public Dictionary<string, string> Report(VehicleDataStore vehicleDataStore)
+    public Dictionary<string, string> Report(VehicleDataStore vehicleDataStore)
     {
-    var metrics = new Dictionary<string, string>();
-    var vehicleStatusCounts = new Dictionary<string, int>();
-    var uniqueDriverIds = new HashSet<int>();
-    var uniqueVehicleIds = new HashSet<int>(); // New HashSet for unique VehicleIds
+        var metrics = new ConcurrentDictionary<string, string>();
+        var vehicleStatusCounts = new ConcurrentDictionary<string, int>();
+        var uniqueDriverIds = new ConcurrentDictionary<int, byte>();
+        var uniqueVehicleIds = new ConcurrentDictionary<int, byte>();
 
-    int totalVehicles = 0;
-    double totalSpeed = 0;
-    double totalAcceleration = 0;
-    double totalEngineRpm = 0;
-    double totalFuelLevel = 0;
-    double totalBrakeUsage = 0;
-    double totalTirePressure = 0;
-    double totalTemperature = 0;
-    int dataPoints = 0;
+        double totalSpeed = 0;
+        double totalAcceleration = 0;
+        double totalEngineRpm = 0;
+        double totalFuelLevel = 0;
+        double totalBrakeUsage = 0;
+        double totalTirePressure = 0;
+        double totalTemperature = 0;
+        int dataPoints = 0;
 
-    foreach (var vehicleDataList in vehicleDataStore.dataStore.Values)
-    {
-        foreach (var vehicleData in vehicleDataList.Values)
+        object lockObject = new object();
+
+        Parallel.ForEach(vehicleDataStore.dataStore.Values, vehicleDataList =>
         {
-            dataPoints++;
-            if (uniqueVehicleIds.Add(vehicleData.VehicleId)) // Check if VehicleId is unique
+            foreach (var vehicleData in vehicleDataList.Values)
             {
-                totalVehicles++; // Increment totalVehicles only if VehicleId is unique
+                lock (lockObject)
+                {
+                    dataPoints++;
+                    if (uniqueVehicleIds.TryAdd(vehicleData.VehicleId, 0))
+                    {
+                        metrics["TotalVehicles"] = (int.Parse(metrics.GetOrAdd("TotalVehicles", "0")) + 1).ToString();
+                    }
+
+                    uniqueDriverIds.TryAdd(vehicleData.DriverId, 0);
+                    totalSpeed += vehicleData.Speed;
+                    totalAcceleration += vehicleData.Acceleration;
+                    totalEngineRpm += vehicleData.EngineRpm;
+                    totalFuelLevel += vehicleData.FuelLevel;
+                    totalBrakeUsage += vehicleData.BrakeUsage;
+                    totalTirePressure += vehicleData.TirePressure;
+                    totalTemperature += vehicleData.Temperature;
+
+                    vehicleStatusCounts.AddOrUpdate(vehicleData.VehicleStatus, 1, (key, oldValue) => oldValue + 1);
+                }
             }
+        });
 
-            uniqueDriverIds.Add(vehicleData.DriverId);
-            totalSpeed += vehicleData.Speed;
-            totalAcceleration += vehicleData.Acceleration;
-            totalEngineRpm += vehicleData.EngineRpm;
-            totalFuelLevel += vehicleData.FuelLevel;
-            totalBrakeUsage += vehicleData.BrakeUsage;
-            totalTirePressure += vehicleData.TirePressure;
-            totalTemperature += vehicleData.Temperature;
+        int totalDrivers = uniqueDriverIds.Count;
 
-            if (vehicleStatusCounts.ContainsKey(vehicleData.VehicleStatus))
-            {
-                vehicleStatusCounts[vehicleData.VehicleStatus]++;
-            }
-            else
-            {
-                vehicleStatusCounts[vehicleData.VehicleStatus] = 1;
-            }
-        }
-    }
+        metrics["TotalDrivers"] = totalDrivers.ToString();
+        metrics["AverageSpeed"] = (totalSpeed / dataPoints).ToString();
+        metrics["AverageAcceleration"] = (totalAcceleration / dataPoints).ToString();
+        metrics["AverageEngineRpm"] = (totalEngineRpm / dataPoints).ToString();
+        metrics["AverageFuelLevel"] = (totalFuelLevel / dataPoints).ToString();
+        metrics["AverageBrakeUsage"] = (totalBrakeUsage / dataPoints).ToString();
+        metrics["AverageTirePressure"] = (totalTirePressure / dataPoints).ToString();
+        metrics["AverageTemperature"] = (totalTemperature / dataPoints).ToString();
+        metrics["MostCommonVehicleStatus"] = vehicleStatusCounts.OrderByDescending(v => v.Value).First().Key;
 
-    int totalDrivers = uniqueDriverIds.Count;
-
-    metrics["TotalVehicles"] = totalVehicles.ToString();
-    metrics["TotalDrivers"] = totalDrivers.ToString();
-    metrics["AverageSpeed"] = (totalSpeed / dataPoints).ToString();
-    metrics["AverageAcceleration"] = (totalAcceleration / dataPoints).ToString();
-    metrics["AverageEngineRpm"] = (totalEngineRpm / dataPoints).ToString();
-    metrics["AverageFuelLevel"] = (totalFuelLevel / dataPoints).ToString();
-    metrics["AverageBrakeUsage"] = (totalBrakeUsage / dataPoints).ToString();
-    metrics["AverageTirePressure"] = (totalTirePressure / dataPoints).ToString();
-    metrics["AverageTemperature"] = (totalTemperature / dataPoints).ToString();
-    metrics["MostCommonVehicleStatus"] = vehicleStatusCounts.OrderByDescending(v => v.Value).First().Key;
-
-    return metrics;
+        return new Dictionary<string, string>(metrics);
     }
 
     public string SaveToFile(VehicleDataStore vehicleDataStore, string directory)
     {
         var data = Report(vehicleDataStore);
         var json = JsonSerializer.Serialize(data);
-        // json name should be datetime .json
         var name = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".json";
         var path = Path.Combine(directory, name);
         File.WriteAllText(path, json);
